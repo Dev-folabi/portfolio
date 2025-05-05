1c8796015c5ccbe2b4f9d033236458858305fc22
afolabi <yusufafolabi95@gmail.com>
2025-05-05 14:02:13 +0100

    build: configure project for GitHub Pages deployment
    Add necessary configurations in next.config.mjs and package.json to enable static export and deployment to GitHub Pages. This includes setting the output to "export", enabling unoptimized images, and adding trailing slashes. Additionally, added scripts for pre-deploy and deploy processes using gh-pages.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +15,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
